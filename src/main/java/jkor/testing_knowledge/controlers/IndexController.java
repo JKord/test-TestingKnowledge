57989f805f41da467751d35a0c96d3754cd1d8fc
JKor <jura.kord@gmail.com>
2015-02-13 01:48:12 +0200

    testing

--- src/main/java/jkor/testing_knowledge/controlers/IndexController.java
+++ src/main/java/jkor/testing_knowledge/controlers/IndexController.java
@@ -5,22 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import jkor.testing_knowledge.entities.Topic;
-import jkor.testing_knowledge.entities.dao.TopicDAO;
-
 import java.util.List;
+import java.util.Set;
+
+import jkor.testing_knowledge.entities.Question;
+import jkor.testing_knowledge.entities.dao.QuestionDAO;
 
 @Controller
 public class IndexController
 {
     @Autowired
-    TopicDAO topicDAO;
+    QuestionDAO questionDAO;
     
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String printWelcome(ModelMap model)
     {
-        List<Topic> t = topicDAO.listTopic();
+        List<Question> t = questionDAO.listQuestion();
+        Set<Integer> s = t.get(0).getCorrectAnswerIds();
         model.addAttribute("message", "Wiiii!");
         return "hello";
     }
--- src/main/java/jkor/testing_knowledge/entities/dao/QuestionDAO.java
+++ src/main/java/jkor/testing_knowledge/entities/dao/QuestionDAO.java
@@ -2,11 +2,13 @@ package jkor.testing_knowledge.entities.dao;
 
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
 import jkor.testing_knowledge.entities.Question;
 
 @Repository
 public class QuestionDAO extends BaseDAO
 {
+    @Transactional
     public List<Question> listQuestion()
     {
         return getCurrentSession().createQuery("from Question").list();
